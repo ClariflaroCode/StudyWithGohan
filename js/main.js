92b4cf3076971e3631e561ffeb3b30c92815d1d9
ClariflaroCode <julietawatts.ar@gmail.com>
2025-04-19 22:39:09 -0300

    Proyecto maquetado

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.getElementById("agregar-tarea").addEventListener("click", agregarTarea);
+let ultimaTarea = 0;
+function agregarTarea(){
+    let nuevaTarea = window.prompt("Nueva tarea: ");
+    let idNuevo = "tarea" + ultimaTarea;
+    let idTareaBoton = "take-tarea" + ultimaTarea;
+    document.getElementById("to-do-list").innerHTML = document.getElementById("to-do-list").innerHTML + `<li id="${idNuevo}">`+nuevaTarea+"</li>"; //+`<button id="${idTareaBoton}" class="tomar-tarea" type="text">Tomar esta tarea </button>`;
+    ultimaTarea++;
+}
+//document.getElementsByClassName("tomar-tarea").addEventListener("click", tomarTarea);
+//function tomarTarea(){
+//    let tareaAPasar = document.getElementById("tarea")
+//}
